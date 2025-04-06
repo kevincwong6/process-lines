aeb0374a23e396576f8cdefdf02783459acca540
Kevin Wong <kevincwong6@gmail.com>
2025-04-05 18:08:26 -0700

    skipped answer line

--- process-lines-1.py
+++ process-lines-1.py
@@ -3,23 +3,13 @@ import sys
 input_file_path = 'data.txt'
 output_file_path = 'output.txt'
 
-found_question_line = False
-question_line = ""
 # Open the input file in read mode and the output file in write mode
 with open(input_file_path, 'r', encoding="utf-8") as infile, open(output_file_path, 'w', encoding="utf-8") as outfile:
     # Read and write each line one by one
     for line in infile:
         
-        if found_question_line:
-            outfile.write(question_line)
-            outfile.write(line + "\n")
-            found_question_line = False
-            print(question_line)
+        if "correct answer" not in line:
+            outfile.write(line)
             print(line)
-            print()
-            #user_input = input("Please enter something: ")
-        elif "Question " in line:
-            question_line = line
-            found_question_line = True
             
        
